b69420db6373d62f2c6f71e61d31ba6c85a0af32
stefano.gianelli@gmail.com <stefano.gianelli@gmail.com@e50cd4d2-4bf1-6755-6efe-ffa46cbba9b3>
2013-12-15 16:10:47 +0000

    Creati Session Bean e interfacce locali sul progetto Client

---
+++ trunk/traveldreamEJB/ejbModule/ejbs/GestoreEscursioneEJB.java
@@ -0,0 +1,18 @@
+package ejbs;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class GestoreEscursioneEJB
+ */
+@Stateless
+public class GestoreEscursioneEJB implements GestoreEscursione {
+
+    /**
+     * Default constructor. 
+     */
+    public GestoreEscursioneEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
