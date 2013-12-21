ba15f1ca10a8dd8d09aa9263be1be0aba8d13a36
stefano.gianelli@gmail.com <stefano.gianelli@gmail.com@e50cd4d2-4bf1-6755-6efe-ffa46cbba9b3>
2013-12-21 14:51:59 +0000

    Implementati metodi del bean profilo

--- trunk/traveldreamWeb/src/beans/ProfiloBean.java
+++ trunk/traveldreamWeb/src/beans/ProfiloBean.java
@@ -1,10 +1,85 @@
 package beans;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.mail.MessagingException;
+
+import dtos.UtenteDTO;
+import ejbs.GestoreProfilo;
 
 @ManagedBean(name="profilo")
 @RequestScoped
 public class ProfiloBean {
+	
+	@EJB
+	private GestoreProfilo profiloBean;
+	
+	private UtenteDTO profilo;
+	
+	@PostConstruct
+	public void setUp () {
+		profilo = new UtenteDTO();
+	}
+
+	public UtenteDTO getProfilo() {
+		return profilo;
+	}
+
+	public void setProfilo(UtenteDTO profilo) {
+		this.profilo = profilo;
+	}
+	
+	/**
+	 * Restituisce il profilo dell'utente che ne fa richiesta
+	 * @return Il DTO dell'utente corrente
+	 */
+	public UtenteDTO getUtente () {
+		return profiloBean.getUtenteCorrente();
+	}
+	
+	/**
+	 * Permette la creazione di un nuovo utente
+	 */
+	public void registrazione () {
+		try {
+			profiloBean.registrazioneUtente(profilo.getEmail());
+		} catch (MessagingException e) {
+			FacesMessage messaggio = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Errore!", "Errore!");
+			FacesContext.getCurrentInstance().addMessage(null, messaggio);
+		}
+	}
+	
+	/**
+	 * Consente l'aggiunta dei dati personali di un utente
+	 * @param datiUtente I dati dell'utente
+	 */
+	public void aggiuntaDatiPersonali (UtenteDTO datiUtente) {
+		profiloBean.aggiuntaDatiPersonali(datiUtente);
+	}
+	
+	/**
+	 * Consente la modifica dei dati personali di un utente
+	 * @param datiUtente I dati dell'utente
+	 */
+	public void modificaDatiPersonali (UtenteDTO datiUtente) {
+		profiloBean.modificaDatiPersonali(datiUtente);
+	}
+	
+	/**
+	 * Permette il reset della passwrod
+	 * @param datiUtente I dati dell'utente
+	 */
+	public void resetPassword (UtenteDTO datiUtente) {
+		try {
+			profiloBean.resetPassword(datiUtente);
+		} catch (MessagingException e) {
+			FacesMessage messaggio = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Errore!", "Errore!");
+			FacesContext.getCurrentInstance().addMessage(null, messaggio);
+		}
+	}
 
 }
