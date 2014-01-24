30aefead94b1b0852002563a3910e2d70306054b
stefano.gianelli@gmail.com <stefano.gianelli@gmail.com@e50cd4d2-4bf1-6755-6efe-ffa46cbba9b3>
2014-01-24 22:17:46 +0000

    Preparazione al rilascio: rimossi elementi di debug e impedito l'accesso alle pagine di test

--- traveldreamEJB/ejbModule/defaultValues.sql
+++ traveldreamEJB/ejbModule/defaultValues.sql
@@ -131,6 +131,8 @@ INSERT INTO collegamenti (tipoCollegamento, origine, destinazione, dataPartenza,
 INSERT INTO collegamenti (tipoCollegamento, origine, destinazione, dataPartenza, oraPartenza, oraArrivo, prezzo, cittaArrivo, cittaPartenza, attivo) VALUES ('AEREO', 'Jose Marti', 'JFK', '2014-03-08', '07:00:00', '20:15:00', 295, 3, 5, 1);
 INSERT INTO collegamenti (tipoCollegamento, origine, destinazione, dataPartenza, oraPartenza, oraArrivo, prezzo, cittaArrivo, cittaPartenza, attivo) VALUES ('AEREO', 'CPT', 'Malpensa', '2014-03-08', '13:30:00', '09:45:00', 510, 1, 9, 1);
 INSERT INTO collegamenti (tipoCollegamento, origine, destinazione, dataPartenza, oraPartenza, oraArrivo, prezzo, cittaArrivo, cittaPartenza, attivo) VALUES ('AEREO', 'RHO', 'Malpensa', '2014-03-08', '10:50:00', '17:00:00', 195, 1, 8, 1);
+INSERT INTO collegamenti (tipoCollegamento, origine, destinazione, dataPartenza, oraPartenza, oraArrivo, prezzo, cittaArrivo, cittaPartenza, attivo) VALUES ('AEREO', 'JFK', 'Jose Marti', '2014-03-08', '08:00:00', '21:15:00', 320, 5, 3, 1);
+INSERT INTO collegamenti (tipoCollegamento, origine, destinazione, dataPartenza, oraPartenza, oraArrivo, prezzo, cittaArrivo, cittaPartenza, attivo) VALUES ('AEREO', 'JFK', 'Jose Marti', '2014-03-08', '12:00:00', '02:15:00', 380, 5, 3, 1);
 
 -- Partenze il 15/03/2014
 INSERT INTO collegamenti (tipoCollegamento, origine, destinazione, dataPartenza, oraPartenza, oraArrivo, prezzo, cittaArrivo, cittaPartenza, attivo) VALUES ('AEREO', 'Fiumicino', 'JFK', '2014-03-15', '11:50:00', '22:35:00', 650, 3, 2, 1);
